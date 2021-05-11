e76459911edbbaee06c6f355557f6cae674fc3bd
Pamicel <paulamicel@gmail.com>
2021-05-11 20:27:39 +0200

    add swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('CEPM backend')
+    .setDescription("L'API du CEPM")
+    .setVersion('1.0-beta')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('explorer', app, document);
   await app.listen(8013);
 }
 bootstrap();
