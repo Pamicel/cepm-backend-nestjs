4050df1a3cdcebbfa763ce1aa1318a8cb87604aa
Pamicel <paulamicel@gmail.com>
2021-06-02 20:23:22 +0200

    add helmet + enable cors

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,12 @@ import { NestFactory, Reflector } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { ClassSerializerInterceptor, ValidationPipe } from '@nestjs/common';
+import * as helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(helmet());
+  app.enableCors();
 
   app.setGlobalPrefix('api');
   app.useGlobalPipes(new ValidationPipe());
