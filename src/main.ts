6cd96a6e440d5be48f8106ff826bd5183a12f743
Pamicel <paulamicel@gmail.com>
2021-06-09 15:53:27 +0200

    make PORT part of the env

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,16 @@ import { NestFactory, Reflector } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { ClassSerializerInterceptor, ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import * as helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(helmet());
   app.enableCors();
+
+  const configService = app.get(ConfigService);
+  const PORT = configService.get<string>('PORT');
 
   app.setGlobalPrefix('api');
   app.useGlobalPipes(new ValidationPipe());
@@ -19,6 +23,6 @@ async function bootstrap() {
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('explorer', app, document);
-  await app.listen(8013);
+  await app.listen(PORT);
 }
 bootstrap();
